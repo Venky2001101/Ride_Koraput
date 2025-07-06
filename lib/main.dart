f83906eb7645599701b00f5b5561b7ad53075fa9
Venky <venkateshchoudhury100@gmail.com>
2025-07-06 12:34:04 +0100

    learning UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:ride_koraput/screens/home_screen.dart';
-import 'screens/splash_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  return runApp(App());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Splash Demo',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SplashScreen(),
-        '/home': (context) => HomeScreen(),
-      },
-    );
+    // TODO: implement build
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,38 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text("Home"));
+    final height = MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            CircleAvatar(radius: 15),
+            SizedBox(width: 10),
+            Text("Cooper"),
+          ],
+        ),
+        actions: [CircleAvatar(radius: 40)],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Hello James", style: TextStyle(fontSize: 30)),
+            Text(
+              "Make your day easy with us",
+              style: TextStyle(fontSize: 15, color: Colors.grey),
+            ),
+            SizedBox(height: 30),
+            SizedBox(height: height * 0.3, child: Row(children: [ 
+
+            ],)),
+          ],
+        ),
+      ),
+    );
   }
 }

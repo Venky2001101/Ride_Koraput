65a3ebe683acc1d2d3740d7237df19d92831dd65
Venky <venkateshchoudhury100@gmail.com>
2025-07-09 19:12:28 +0100

    Add ProfileScreen with navigation and user details

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ride_koraput/screens/dummy_screen.dart';
 import 'package:ride_koraput/screens/home_screen.dart';
+import 'package:ride_koraput/screens/profile_screen.dart';
 
 void main() {
   return runApp(App());
@@ -12,11 +13,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
+      // theme: ThemeData.light(),
+      // darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-      home: DummyScreen(),
+      home: ProfileScreen(),
     );
   }
 }
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  int _selectedindex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedindex = index;
+    });
+  }
+
+  final widgetList = [ProfilePage(), Container(), Container()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widgetList[_selectedindex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedindex,
+        onTap: _onItemTapped,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.call), label: "call"),
+          BottomNavigationBarItem(icon: Icon(Icons.edit), label: "Edit"),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  ProfilePage({super.key});
+
+  final List<Map<String, dynamic>> lists = [
+    {"name": "Personal Data", "icon": Icons.person},
+    {"name": "settings", "icon": Icons.settings},
+    {"name": "E-Statement", "icon": Icons.chat},
+    {"name": "Referal Code", "icon": Icons.favorite},
+  ];
+  final List<Map<String, dynamic>> list2 = [
+    {"name": "FAQs", "icon": Icons.more_horiz},
+    {"name": "Our Handbook", "icon": Icons.edit_square},
+    {"name": "Community", "icon": Icons.groups_3},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        alignment: Alignment.topCenter,
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/profile.jpg'),
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    // child: Image.asset('assets/profile.jpg', fit: BoxFit.fill),
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "William Jhon malik",
+                        style: TextStyle(fontSize: 20),
+                      ),
+
+                      Text("Aggresive Invester"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            SizedBox(height: 20),
+            ListView.separated(
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> k = lists[index];
+                return Container(
+                  height: 40,
+                  decoration: BoxDecoration(),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: const Color.fromARGB(
+                          255,
+                          246,
+                          241,
+                          241,
+                        ),
+                        child: Icon(k["icon"], color: Colors.black, size: 25),
+                      ),
+                      SizedBox(width: 10),
+                      Text(k["name"]),
+                      Spacer(),
+                      Icon(Icons.arrow_forward_ios_rounded, size: 20),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 10);
+              },
+              itemCount: lists.length,
+            ),
+
+            Divider(height: 30),
+
+            ListView.separated(
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> k2 = list2[index];
+                return Container(
+                  height: 40,
+                  decoration: BoxDecoration(),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: const Color.fromARGB(
+                          255,
+                          246,
+                          241,
+                          241,
+                        ),
+                        child: Icon(k2["icon"], color: Colors.black, size: 25),
+                      ),
+                      SizedBox(height: 10),
+                      SizedBox(width: 10),
+                      Text(k2['name']),
+                      Spacer(),
+                      Icon(Icons.arrow_forward_ios_rounded, size: 20),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 10);
+              },
+              itemCount: list2.length,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
